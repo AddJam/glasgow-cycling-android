568998041b5d5fa37beaefc7092a462c049dbd7a
mdhayes <mdhayes@gmx.com>
2014-07-02 14:34:58 +0100

    addeding jc colors to xml value

--- app/src/main/java/com/fcd/glasgowcycling/activities/SignInActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/SignInActivity.java
@@ -48,7 +48,6 @@ public class SignInActivity extends Activity {
         //signUpButton.setOnClickListener(new SignUpListener()); Have to do this
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
