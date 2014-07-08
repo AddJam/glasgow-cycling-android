b1500a38abde11cf6d119a062994a407a5b68d5e
Chris Sloey <chris.sloey@gmail.com>
2014-07-08 14:14:15 +0100

    Dev settings for client_id and client_secret

--- app/src/main/java/com/fcd/glasgowcycling/api/http/ApiClientModule.java
+++ app/src/main/java/com/fcd/glasgowcycling/api/http/ApiClientModule.java
@@ -41,7 +41,7 @@ public class ApiClientModule {
 
         final RestAdapter restAdapter = new RestAdapter.Builder()
 //                .setEndpoint("http://10.0.2.2:3000") // Localhost (for simulator)
-                .setEndpoint("http://172.20.10.8:3000") // Tethered IP (device)
+                .setEndpoint("http://172.20.10.5:3000") // Tethered IP (device)
                 .setConverter(new GsonConverter(gson))
                 .setRequestInterceptor(new RequestInterceptor() {
                     @Override
