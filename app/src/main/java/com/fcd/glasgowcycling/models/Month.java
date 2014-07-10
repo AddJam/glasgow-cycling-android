61fd439bc37b05f6575f27f69bbff9d345547766
Chris Sloey <chris.sloey@gmail.com>
2014-07-10 14:29:15 +0100

    Use model to generate stat text

--- app/src/main/java/com/fcd/glasgowcycling/models/Month.java
+++ app/src/main/java/com/fcd/glasgowcycling/models/Month.java
@@ -25,12 +25,29 @@ public class Month {
         return km;
     }
 
+    public Double getMiles() {
+        return km * 0.621371192;
+    }
+
     public void setKm(Double km) {
         this.km = km;
     }
 
     public Integer getSeconds() {
         return seconds;
+    }
+
+    public Integer getHours() {
+        return (int)Math.floor(seconds / 3600);
+    }
+
+    public Integer getMinutes() {
+        int minutesInSeconds = seconds - (getHours() * 3600);
+        return (int)Math.floor(minutesInSeconds / 60);
+    }
+
+    public String getReadableDistance() {
+        return String.valueOf(getHours()) + ":" + String.valueOf(getMinutes()) + " hours";
     }
 
     public void setSeconds(Integer seconds) {
