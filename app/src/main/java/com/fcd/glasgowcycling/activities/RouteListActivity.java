c53ed09eda8e5d09b031f35647e955db6a8273ed
Chris Sloey <chris.sloey@gmail.com>
2014-07-17 11:59:22 +0100

    Route list with very basic layout

--- app/src/main/java/com/fcd/glasgowcycling/activities/RouteListActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/RouteListActivity.java
@@ -7,7 +7,13 @@ import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.fcd.glasgowcycling.R;
+import com.fcd.glasgowcycling.adapters.RouteAdapter;
+import com.fcd.glasgowcycling.models.Route;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class RouteListActivity extends Activity {
@@ -18,5 +24,17 @@ public class RouteListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.route_list);
+        ButterKnife.inject(this);
+        List<Route> routes = new ArrayList<Route>();
+        routes.add(getRoute());
+        routes.add(getRoute());
+        routes.add(getRoute());
+        routesList.setAdapter(new RouteAdapter(this, R.layout.route_cell, routes));
+    }
+
+    public Route getRoute() {
+        Route r = new Route();
+        r.setName("pew");
+        return r;
     }
 }
--- app/src/main/java/com/fcd/glasgowcycling/adapters/RouteAdapter.java
+++ app/src/main/java/com/fcd/glasgowcycling/adapters/RouteAdapter.java
@@ -1,9 +1,66 @@
 package com.fcd.glasgowcycling.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fcd.glasgowcycling.R;
+import com.fcd.glasgowcycling.models.Route;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by chrissloey on 16/07/2014.
  */
-public class RouteAdapter {
+public class RouteAdapter extends ArrayAdapter<Route> {
+
+    public RouteAdapter(Context context, int resourceId, List<Route> routes) {
+        super(context, resourceId, routes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        ViewHolder holder; // to reference the child views for later actions
+
+        if (v == null) {
+            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.route_cell, null);
+            // cache view fields into the holder
+            holder = new ViewHolder(v);
+            // associate the holder with the view for later lookup
+            v.setTag(holder);
+        }
+        else {
+            // view already exists, get the holder instance from the view
+            holder = (ViewHolder) v.getTag();
+        }
+
+        holder.toName.setText("Pew");
+
+        return v;
+    }
+
+    // somewhere else in your class definition
+    static class ViewHolder {
+        @InjectView(R.id.toName) TextView toName;
+        @InjectView(R.id.fromName) TextView fromName;
+        @InjectView(R.id.rating) TextView rating;
+        @InjectView(R.id.numReviews) TextView numReviews;
+        @InjectView(R.id.averageDistance) TextView averageDistance;
+        @InjectView(R.id.averageTime) TextView averageTime;
+        @InjectView(R.id.numInstances) TextView numInstances;
+        @InjectView(R.id.typeImage) ImageView typeImage;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
 }
