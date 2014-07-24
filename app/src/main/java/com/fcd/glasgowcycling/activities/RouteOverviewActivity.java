bb766ac22797e99d1f619a5c4ce21efc33c071fe
Chris Sloey <chris.sloey@gmail.com>
2014-07-24 14:18:22 +0100

    Fix up navigation

--- app/src/main/java/com/fcd/glasgowcycling/activities/RouteOverviewActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/RouteOverviewActivity.java
@@ -14,22 +14,13 @@ public class RouteOverviewActivity extends Activity {
         setContentView(R.layout.activity_route_overview);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.route_overview, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                this.finish();
+                return (true);
         }
         return super.onOptionsItemSelected(item);
     }
