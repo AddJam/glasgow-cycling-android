b484d58c80a7b88c5accad907369ce1d05db273c
Chris Sloey <chris.sloey@gmail.com>
2014-07-28 15:15:05 +0100

    Data presented in route overview, readable and abstracted from activity

--- app/src/main/java/com/fcd/glasgowcycling/activities/RouteOverviewActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/RouteOverviewActivity.java
@@ -5,20 +5,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
 import com.fcd.glasgowcycling.R;
 import com.fcd.glasgowcycling.models.Route;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class RouteOverviewActivity extends Activity {
 
     private final String TAG = "Route Overview";
 
+    @InjectView(R.id.rating) RatingBar ratingBar;
+    @InjectView(R.id.time) TextView time;
+    @InjectView(R.id.speed) TextView speed;
+    @InjectView(R.id.distance) TextView distance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_overview);
+        ButterKnife.inject(this);
+
         Bundle bundle = getIntent().getExtras();
         Route route = (Route)bundle.getSerializable("route");
         Log.d(TAG, "Route is from " + route.getStartName());
+
+        ratingBar.setRating(route.getAverages().getRating().floatValue());
+        time.setText(route.getAverages().getReadableTime());
+        speed.setText(route.getAverages().getReadableSpeed());
+        distance.setText(route.getAverages().getReadableDistance());
     }
 
     @Override
