191c03f9124dda615fc28008cdb8554620eeca85
Chris Sloey <chris.sloey@gmail.com>
2014-07-29 14:09:19 +0100

    Abstract route click listener

--- app/src/main/java/com/fcd/glasgowcycling/activities/RouteListActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/RouteListActivity.java
@@ -12,6 +12,7 @@ import android.widget.ListView;
 
 import com.fcd.glasgowcycling.CyclingApplication;
 import com.fcd.glasgowcycling.R;
+import com.fcd.glasgowcycling.adapters.OnRouteClickListener;
 import com.fcd.glasgowcycling.adapters.RouteAdapter;
 import com.fcd.glasgowcycling.api.http.GoCyclingApiInterface;
 import com.fcd.glasgowcycling.models.Route;
@@ -32,7 +33,7 @@ public class RouteListActivity extends Activity {
 
     private final String TAG = "RouteList";
 
-    private List<Route> routes;
+    private OnRouteClickListener mRouteClickListener;
     @InjectView(R.id.route_list) ListView routesList;
     @Inject GoCyclingApiInterface cyclingService;
 
@@ -43,29 +44,18 @@ public class RouteListActivity extends Activity {
         ((CyclingApplication) getApplication()).inject(this);
         ButterKnife.inject(this);
 
-        routesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Route route = routes.get(position);
-                if (route != null) {
-                    Intent overviewIntent = new Intent(getBaseContext(), RouteOverviewActivity.class);
-                    Bundle extras = new Bundle();
-                    extras.putSerializable("route", route);
-                    overviewIntent.putExtras(extras);
-                    startActivity(overviewIntent);
-                }
-            }
-        });
-
-        if (routes != null) {
-            routesList.setAdapter(new RouteAdapter(getBaseContext(), R.layout.route_cell, routes));
+        if (mRouteClickListener != null) {
+            routesList.setOnItemClickListener(mRouteClickListener);
+            routesList.setAdapter(new RouteAdapter(getBaseContext(), R.layout.route_cell, mRouteClickListener.getRoutes()));
         } else {
+            mRouteClickListener = new OnRouteClickListener(this);
+            routesList.setOnItemClickListener(mRouteClickListener);
             cyclingService.searchRoutes(true, new Callback<RouteList>() {
                 @Override
                 public void success(RouteList routeList, Response response) {
                     Log.d(TAG, "Got routes - total: " + routeList.getRoutes().size());
-                    routes = routeList.getRoutes();
-                    routesList.setAdapter(new RouteAdapter(getBaseContext(), R.layout.route_cell, routes));
+                    mRouteClickListener.setRoutes(routeList.getRoutes());
+                    routesList.setAdapter(new RouteAdapter(getBaseContext(), R.layout.route_cell, routeList.getRoutes()));
                 }
 
                 @Override
