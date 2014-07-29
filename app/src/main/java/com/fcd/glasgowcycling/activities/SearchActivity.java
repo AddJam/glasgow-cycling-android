e4e87fe27b78497eb575638282ea6068ad2fe132
mdhayes <mdhayes@gmx.com>
2014-07-29 14:36:48 +0100

    styling changes and strings more descriptive for search

--- app/src/main/java/com/fcd/glasgowcycling/activities/SearchActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/SearchActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.fcd.glasgowcycling.CyclingApplication;
@@ -64,5 +65,21 @@ public class SearchActivity extends Activity {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(this.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
