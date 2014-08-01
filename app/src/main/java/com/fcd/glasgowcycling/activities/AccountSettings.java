3e288c68726943ffce665c4906ac37e400c310ec
mdhayes <mdhayes@gmx.com>
2014-08-01 13:29:34 +0100

    fleshed out more of the view, setting values from user model, adding some actions to the buttons - wet from sign up

--- app/src/main/java/com/fcd/glasgowcycling/activities/AccountSettings.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/AccountSettings.java
@@ -1,16 +1,30 @@
 package com.fcd.glasgowcycling.activities;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.activeandroid.query.Select;
 import com.fcd.glasgowcycling.R;
 import com.fcd.glasgowcycling.api.http.GoCyclingApiInterface;
 import com.fcd.glasgowcycling.models.User;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import javax.inject.Inject;
 
@@ -22,13 +36,17 @@ public class AccountSettings extends Activity {
     GoCyclingApiInterface cyclingService;
     private static final String TAG = "AccountSettings";
 
-    @InjectView(R.id.distance_info) TextView distanceInfo;
-    @InjectView(R.id.time_info) TextView timeInfo;
-    @InjectView(R.id.avg_speed_info) TextView avgSpeedInfo;
-    @InjectView(R.id.speed_info) TextView speedInfo;
-    @InjectView(R.id.finish_button) Button finishButton;
+    @InjectView(R.id.settings_last_name) EditText firstNameField;
+    @InjectView(R.id.settings_first_name) EditText lastNameField;
+    @InjectView(R.id.settings_email) EditText emailField;
+    @InjectView(R.id.settings_picture_button) Button pictureButton;
+    @InjectView(R.id.gender_button) Button genderButton;
+    @InjectView(R.id.settings_submit_button) Button submitButton;
+    @InjectView(R.id.settings_logout_button) Button logoutButton;
 
     private User mUser;
+
+    private static final int SELECT_PHOTO = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +55,51 @@ public class AccountSettings extends Activity {
 
         mUser = new Select().from(User.class).limit(1).executeSingle();
 
+        firstNameField.setText(mUser.getFirstName());
+        lastNameField.setText(mUser.getLastName());
+        emailField.setText(mUser.getEmail());
+
+        if (mUser.getProfilePic() != null){
+            byte[] decodedString = Base64.decode(mUser.getProfilePic(), Base64.DEFAULT);
+            Bitmap decodedImage = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            Drawable drawableImage = new BitmapDrawable(getResources(),decodedImage);
+            pictureButton.setBackground(drawableImage);
+        }
+        genderButton.setText(mUser.getGender());
+
+        genderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Gender clicked");
+                genderPicker();
+            }
+        });
+
+        pictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Picture button clicked");
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Submit settings clicked");
+                //submit settings
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Submit sign up clicked");
+                //logout();
+            }
+        });
     }
 
 
@@ -57,5 +120,67 @@ public class AccountSettings extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void genderPicker(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.gender_dialog);
+        dialog.setTitle("Select a gender");
+        dialog.setCancelable(true);
+
+        Button undisclosedButton = (Button) dialog.findViewById(R.id.undisclosed_button);
+        Button femaleButton = (Button) dialog.findViewById(R.id.female_button);
+        Button maleButton = (Button) dialog.findViewById(R.id.male_button);
+
+        undisclosedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Undisclosed Gender selected");
+                genderButton.setText("Undisclosed");
+                dialog.dismiss();
+            }
+        });
+        femaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Female Gender selected");
+                genderButton.setText("Female");
+                dialog.dismiss();
+            }
+        });
+        maleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Male Gender selected");
+                genderButton.setText("Male");
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    InputStream imageStream = null;
+                    Bitmap userSelectedImage;
+                    try {
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    userSelectedImage = BitmapFactory.decodeStream(imageStream);
+                    if (userSelectedImage.getWidth() > 400 || userSelectedImage.getHeight() > 400){
+                        userSelectedImage = Bitmap.createBitmap(userSelectedImage, (userSelectedImage.getWidth() / 2) - 200, (userSelectedImage.getHeight() / 2) - 200, 400, 400);
+                    }
+                    Drawable drawableImage = new BitmapDrawable(getResources(), userSelectedImage);
+                    pictureButton.setBackground(drawableImage);
+                }
+        }
     }
 }
