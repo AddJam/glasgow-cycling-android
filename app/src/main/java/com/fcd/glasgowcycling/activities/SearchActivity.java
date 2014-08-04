0bfea4a72cf4d8fde7fde65e86dd1357cae9341a
Chris Sloey <chris.sloey@gmail.com>
2014-08-04 10:35:01 +0100

    Search works with reverse geocoding

--- app/src/main/java/com/fcd/glasgowcycling/activities/SearchActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/SearchActivity.java
@@ -3,6 +3,8 @@ package com.fcd.glasgowcycling.activities;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,6 +19,9 @@ import com.fcd.glasgowcycling.adapters.RouteAdapter;
 import com.fcd.glasgowcycling.api.http.GoCyclingApiInterface;
 import com.fcd.glasgowcycling.models.RouteList;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.ButterKnife;
@@ -27,11 +32,13 @@ import retrofit.client.Response;
 
 public class SearchActivity extends RouteListActivity {
     private final String TAG = "Search";
+    private Geocoder mGeoCoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mGeoCoder = new Geocoder(this);
         handleIntent(getIntent());
     }
 
@@ -45,9 +52,16 @@ public class SearchActivity extends RouteListActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
 
-            float lat = 55.4f;
-            float lng = -4.29f;
-            search(false, lat, lng);
+            try {
+                List<Address> geoInfo = mGeoCoder.getFromLocationName(query, 1);
+                Address location = geoInfo.get(0);
+                if (location.hasLatitude() && location.hasLongitude()) {
+                    Log.d(TAG, "Searching for routes near " + location.getLatitude() + ", " + location.getLongitude());
+                    search(false, (float)location.getLatitude(), (float)location.getLongitude());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
