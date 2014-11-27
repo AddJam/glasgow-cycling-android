c3e1d5308ec03ee6c5be6503d555105a13fa3f10
mdhayes <mdhayes@gmx.com>
2014-11-27 10:53:53 +0000

    remove menu

--- app/src/main/java/com/fcd/glasgowcycling/activities/UserStatsActivity.java
+++ app/src/main/java/com/fcd/glasgowcycling/activities/UserStatsActivity.java
@@ -96,26 +96,6 @@ public class UserStatsActivity extends Activity {
         getStats();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.user_stats, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public void getStats(){
         // Load from API
         int numDays = 7;
